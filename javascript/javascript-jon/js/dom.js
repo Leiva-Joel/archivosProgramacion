b1825e508b79198cd93339f50d6beee8245087c1
Joel Leiva <leivajoel79@gmail.com>
2022-02-20 01:04:02 -0300

    actualize dom.js 6

--- javascript/javascript-jon/js/dom.js
+++ javascript/javascript-jon/js/dom.js
@@ -277,7 +277,7 @@ $card.classList.remove("opacity-80", "sepia")
 //Toggle no acepta varias clases, hay que aplicarlas una por una
 $card.classList.toggle("opacity-80", "sepia")
 
- */
+*/
 
 
 
@@ -371,3 +371,84 @@ console.log($cards.closest("body"))
 console.log($cards.children[3].closest("section")) 
 
 */
+
+// <=============================68.DOM: Creando Elementos y Fragmentos=============================>
+
+//Crear una card desde JS
+const $figure = document.createElement("figure"),
+  $img = document.createElement("img"),
+  $figCaption = document.createElement("figcaption"),
+  $figCaptionText = document.createTextNode("Animals"),
+  $cards = document.querySelector(".cards"),
+  $figure2 = document.createElement("figure");
+
+$img.setAttribute("src", "https://placeimg.com/200/200/animals")
+$img.setAttribute("alt", "Animals")
+
+$figCaption.appendChild($figCaptionText)
+$figure.classList.add("card")
+//Le crea un figure al hijo
+$figure.appendChild($img)
+$figure.appendChild($figCaption)
+
+$cards.appendChild($figure);
+
+$figure2.innerHTML = `
+  <img src="https://placeimg.com/200/200/people" alt="People" />
+  <figcaption>People</figcaption>
+`
+
+$figure2.classList.add("card")
+$cards.appendChild($figure2)
+
+//Forma de agreagar elementos dinamicamente con JS
+const estaciones = ["Primavera", "Verano", "Otoño", "Invierno"],
+  $ul = document.createElement("ul")
+
+document.write("<h3>Estaciones del Año</h3>")
+document.body.appendChild($ul)
+
+estaciones.forEach(el => {
+  const $li = document.createElement("li")
+  $li.textContent = el
+  $ul.appendChild($li)
+})
+
+//Otra forma de agregar elementos dinamicamente con JS
+
+const continentes = ["Africa", "America", "Asia", "Europa", "Oceania"],
+  $ul2 = document.createElement("ul")
+
+document.write("<h3>Contienentes del Mundo</h3>")
+document.body.appendChild($ul2)
+$ul2.innerHTML = ""
+continentes.forEach((el) => ($ul2.innerHTML += `<li>${el}</li>`))
+
+//Mejor forma de agregar elementos dinamicamente con JS, asi le gastamos menos recursos al navegador
+
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre"
+],
+  $ul3 = document.createElement("ul"),
+  $fragment = document.createDocumentFragment();
+
+meses.forEach((el) => {
+  const $li = document.createElement("li");
+  $li.textContent = el;
+  $fragment.appendChild($li);
+});
+
+document.write("<h3>Meses del Año</h3>");
+$ul3.appendChild($fragment);
+document.body.appendChild($ul3);
