1e6a397e6f4f064067a8c73649b9aeeb28855648
Joel Leiva <leivajoel79@gmail.com>
2022-02-26 01:01:32 -0300

    agregue menu_hamburguesa.js

--- javascript/javascript-jon/js/dom/menu-hamburguesa.js
+++ javascript/javascript-jon/js/dom/menu-hamburguesa.js
@@ -11,15 +11,3 @@ export default function hamburgerMenu(menuBtn, menu, menuLink) {
     }
   })
 }
-
-/* const $menuIcon = document.querySelector(".nav__icon"),
-  $menu = document.querySelector(".menu"),
-  $menuLinks = document.querySelector(".menu__link")
-
-$menuIcon.addEventListener("click", e => {
-  $menu.classList.toggle("menu--show")
-})
-
-$menuLinks.addEventListener("click", e => {
-  $menu.classList.toggle("menu--show")
-}) */
